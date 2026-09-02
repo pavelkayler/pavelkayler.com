a735180c5a372d12906aef849ed136ac83ccba70
kayler <pavkokovin@gmail.com>
2026-09-02 08:45:09 +0300

    Route Works and Contacts through native React pages

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { MainLayout } from '../layouts/MainLayout'
 import { LegacyPage } from '../components/LegacyPage'
+import { WorksPage } from '../pages/WorksPage'
+import { ContactsPage } from '../pages/ContactsPage'
 
 const basename = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
 
@@ -10,11 +12,11 @@ export const router = createBrowserRouter(
       element: <MainLayout />,
       children: [
         { index: true, element: <LegacyPage pageKey="home" /> },
-        { path: 'works', element: <LegacyPage pageKey="works" /> },
+        { path: 'works', element: <WorksPage /> },
         { path: 'portraits', element: <LegacyPage pageKey="portraits" /> },
         { path: 'projects', element: <LegacyPage pageKey="projects" /> },
         { path: 'brands', element: <LegacyPage pageKey="brands" /> },
-        { path: 'contacts', element: <LegacyPage pageKey="contacts" /> },
+        { path: 'contacts', element: <ContactsPage /> },
         { path: '*', element: <Navigate to="/" replace /> },
       ],
     },
