a2921eb01a423aad94650acc7dc537a4315b8d33
kayler <pavkokovin@gmail.com>
2026-09-11 06:10:15 +0300

    Place a static home logo above the hero copy

--- src/components/HomeSlider.tsx
+++ src/components/HomeSlider.tsx
@@ -4,6 +4,7 @@ import { imageUrl, requestImage, subscribeViewport, viewportSnapshot } from '../
 import type { HomeContent } from '../content/types'
 import { usePrefersReducedMotion } from '../hooks/usePrefersReducedMotion'
 import { LogoSpacer } from './LogoSpacer'
+import { SiteLogo } from './SiteLogo'
 import { StructuredImage } from './StructuredImage'
 
 export function HomeSlider({ cover }: { cover: HomeContent['cover'] }) {
@@ -47,6 +48,7 @@ export function HomeSlider({ cover }: { cover: HomeContent['cover'] }) {
       <div className="cover-wrapper js-cover-wrapper -has-arrows">
         <LogoSpacer coverSize="medium" />
         <div className="cover-content">
+          <div className="home-site-logo"><SiteLogo /></div>
           <h1 className="cover-header -medium">{cover.title}</h1><p>{cover.subtitle}</p>
         </div>
         <div className="cover-aside">
